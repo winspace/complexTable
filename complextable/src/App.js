5a69f84c970e2ca8af6b665917f6f65ef6c5f77e
yanhuinan <yanhuinan@unionbigdata.com>
2019-04-07 18:08:02 +0800

    插入行列 删除行列

--- complextable/src/App.js
+++ complextable/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import TableContainer from "./components/tableContainer";
 import "antd/dist/antd.css";
 import Handsontable from "handsontable";
+import { Col } from "antd";
 
 class App extends Component {
   state = {
@@ -15,9 +16,28 @@ class App extends Component {
       startRows: 0,
       startCols: 0,
       contextMenu: {
+        //https://handsontable.com/docs/7.0.0/demo-context-menu.html?_ga=2.206387603.1212794906.1554629973-1787957690.1553829183#page-default
         items: {
           row_above: {
-            name: "插入一行"
+            name: "上方插入一行"
+          },
+          row_below: {
+            name: "下方插入一行"
+          },
+          col_left: {
+            name: "左侧插入一列"
+          },
+          col_right: {
+            name: "右侧插入一列"
+          },
+          remove_row: {
+            name: "删除行"
+          },
+          remove_col: {
+            name: "删除列"
+          },
+          clear_column: {
+            name: "清空列"
           },
           // row_below: {
           //   name: "Insert row above this one (custom name)"
@@ -63,14 +83,17 @@ class App extends Component {
       settings: tempSetting
     });
   };
-  fillDataFull = ()=>{
+  fillDataFull = () => {
     const tempSetting = this.state.settings;
-  
-    tempSetting.data = Handsontable.helper.createSpreadsheetData(tempSetting.startRows, tempSetting.startCols);
+
+    tempSetting.data = Handsontable.helper.createSpreadsheetData(
+      tempSetting.startRows,
+      tempSetting.startCols
+    );
     this.setState({
       settings: tempSetting
     });
-  }
+  };
 
   render() {
     return (
@@ -80,7 +103,7 @@ class App extends Component {
           createNewTable={this.createNewTable}
           toggleRowHeader={this.toggleRowHeader}
           toggleColHeader={this.toggleColHeader}
-          fillDataFull = {this.fillDataFull}
+          fillDataFull={this.fillDataFull}
         />
       </div>
     );
